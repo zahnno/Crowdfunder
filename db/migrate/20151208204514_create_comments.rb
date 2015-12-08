855106deca9f2e985a07c0adbbb980aab195f642
PatrickZanowski <zanowski26@hotmail.com>
2015-12-08 15:59:26 -0500

    Fixed table issue

--- db/migrate/20151208204514_create_comments.rb
+++ db/migrate/20151208204514_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.text :text
-      t.interger :user_id
+      t.integer :user_id
       t.integer :comment_id
 
       t.timestamps null: false
