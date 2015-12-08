096c27149e356e8ccefbce4360d90ebef80f4cd5
PatrickZanowski <zanowski26@hotmail.com>
2015-12-08 17:07:10 -0500

    Added Routes

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -1,12 +1,18 @@
 class PledgesController < ApplicationController
+before_filter :ensure_logged_in, only: [:create, :destroy]
+before_filter :load_project
+
+def new
+   @pledge = Pledge.new
+end
 
 def create
+   @pledge = Pledge.new(param[:id])
 end
 
-def update
-end
-
-def destroy
-end
+private 
+  def review_params
+  	params.require(:review).permit(:comment, :product_id)
+  end
 
 end
