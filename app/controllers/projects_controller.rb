6d209a338a6ff11e2a4bccedd5d99f9fa8a0672c
Mike Cooper <shivagogo@gmail.com>
2015-12-08 17:32:36 -0500

    Create empty methods in projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,30 @@
 class ProjectsController < ApplicationController
+	def index
+
+	end
+
+	def show
+
+	end
+
+	def new
+
+	end
+
+	def edit
+
+	end
+
+	def create
+
+	end
+
+	def update
+
+	end
+
+	def destroy
+
+	end
 end
+
