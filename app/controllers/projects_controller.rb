af064422bda651de25bcdd41743918f0982e9242
swiftyy188 <jeffho.ml@gmail.com>
2015-12-08 22:02:17 -0500

    usercontrollers done

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,48 @@
 class ProjectsController < ApplicationController
+	def index
+		@projects = Project.all
+	end
+
+	def show
+		@product = Project.find(params[:id])
+	end
+
+	def new
+		@project = Project.new
+	end
+
+	def edit
+		@product = Project.find(params[:id])
+	end
+
+	def create
+		@project = Project.new(project_params)
+
+		if @project.save
+			redirect_to projects_path
+		else
+			render :new
+		end
+	end
+
+	def update
+		@project = Project.find(params[:id])
+
+		if @project.update_attributes(project_params)
+			redirect_to project_path(@project)
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@project = Project.find(params[:id])
+  	@project.destroy
+  	redirect_to projects_path
+	end
+
+	private
+		def project_params
+			params.require(:project).permit(:name, :description, :start_date, :end_date, :funding_goal, :owner_id, rewards_attributes: [:title, :description, :amount, :project_id, :_destroy])
+	end
 end
