f1408f56ea413046a7249af8a5bf1f61e4611776
Mike Cooper <shivagogo@gmail.com>
2015-12-10 17:58:14 -0500

    Create category routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
 
 root'projects#index'
-resources :projects do
-	resources :comments, only: [:create, :destroy]
-	resources :pledges, only: [:create, :show, :new]
-end
 
-resources :users, only: [:new, :create, :show]
-resources :sessions, only: [:new, :create, :destroy]
+	resources :categories
+
+	resources :projects do
+		resources :comments, only: [:create, :destroy]
+		resources :pledges, only: [:create, :show, :new]
+	end
+
+	resources :users, only: [:new, :create, :show]
+	resources :sessions, only: [:new, :create, :destroy]
 
 end
