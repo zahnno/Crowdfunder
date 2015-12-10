7520c472a2db7292ce72d87ac7f0818e2c1201ef
PatrickZanowski <zanowski26@hotmail.com>
2015-12-10 16:22:46 -0500

    Assigned user id to created projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -22,7 +22,8 @@ class ProjectsController < ApplicationController
 
 	def create
 		@project = Project.new(project_params)
-		
+		@project.owner_id = current_user.id
+
 		if @project.save
 			redirect_to projects_path
 		else
