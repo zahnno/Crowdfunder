e7de15f5298d6b411a58ebea6ed789e9ae406ef3
PatrickZanowski <zanowski26@hotmail.com>
2015-12-11 12:13:42 -0500

    Added styling to application and index

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,8 +4,7 @@ class ProjectsController < ApplicationController
 	end
 
 	def show
-		@project = Project.find(params[:id])
-
+		@project = Project.find(params[:id]) 
 		if current_user
      		@comment = Comment.new
     	end
