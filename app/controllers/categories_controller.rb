ddb059365ccdb07f26282c3ca32540c2ff9d319d
PatrickZanowski <zanowski26@hotmail.com>
2015-12-11 14:09:51 -0500

    Added new styling to project index and categories index

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -13,7 +13,7 @@ class CategoriesController < ApplicationController
 
 	private
 	def category_params
-		params.require(:category).permit(:name, :category_id)
+		params.require(:category).permit(:name, :category_id, :image)
 	end
 
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,7 @@
 class ProjectsController < ApplicationController
 	def index
 		@projects = Project.all
+		@categories = Category.all
 	end
 
 	def show
