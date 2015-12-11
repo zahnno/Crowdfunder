2c3b94e490f8b7f007e93a09cbe36949dbbe3c4e
PatrickZanowski <zanowski26@hotmail.com>
2015-12-10 21:07:57 -0500

    User can see owner info

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -22,7 +22,7 @@ class ProjectsController < ApplicationController
 
 	def create
 		@project = Project.new(project_params)
-		@project.owner_id = current_user.id
+		@project.user = current_user
 
 		if @project.save
 			redirect_to projects_path
